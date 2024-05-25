2edf5fe89a9cede0676cac13cd7963bee185e48e
Kitiphat.Koptor <104866171+Kitiphat@users.noreply.github.com>
2024-05-25 21:49:41 +0700

    change API URL fixed deployed backend and frontend sending data

--- ai-tpr-proj/src/function/chatContent.js
+++ ai-tpr-proj/src/function/chatContent.js
@@ -1,27 +1,26 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const BASE_URL = process.env.REACT_APP_BASE_API_URL;
 
 export const storeContent = async (chatData) => {
   const token = localStorage.getItem('token');
-  
+
   try {
     console.log("storeContent", chatData);
     const response = await axios.post(
-      `${BASE_URL}api/storechatdata`,
+      `${BASE_URL}/api/storechatdata`,
       chatData,
       {
         headers: {
           "Content-Type": "application/json",
-          "authtoken": token // Include the token in the request headers
+          "authtoken": token
         },
       }
-    );  // send data to server
-    console.log("log from line 18 chatContent.js",response.data); // Log response data
+    );
+    console.log("log from line 18 chatContent.js", response.data);
     return response.data;
   } catch (error) {
     console.log("Error sending data!", error);
     return { error: error.response.data || "Error storing data!" };
   }
 };
-
